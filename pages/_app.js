79c508e3e0cdbeb4ab03a8033230d64f683d4e68
Huy Khuong <scottkhuong@gmail.com>
2022-05-28 10:17:03 +0700

    28/05

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
 import "../styles/globals.css";
+import GoogleAnalytics from "@bradgarropy/next-google-analytics";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0,minimum-scale=1height=device-height,viewport-fit=cover, shrink-to-fit=no"
+        />
+        <meta name="theme-color" content="#3A563E"></meta>
+        <meta property="og:image" content="/logo.png"></meta>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c"></meta>
+      </Head>
+
+      <GoogleAnalytics
+        measurementId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
